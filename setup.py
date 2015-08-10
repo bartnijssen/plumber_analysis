daf57aaa1c4ce8aa9216330e500258a30d54491d
Bart Nijssen <nijssen@uw.edu>
2015-08-10 14:11:13 -0600

    Setup script for python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='plumber_analysis',
+      version='0.01',
+      description='Analysis package for PLUMBER data',
+      author='Bart Nijssen',
+      author_email='nijssen@uw.edu',
+      url='http://www.github.com/bartnijssen/plumber_analysis',
+      packages=['plumber'],
+      py_modules=['plumber.io', 'plumber.config', 'plumber.plumber',
+                  'plumber.plot'],
+     )
